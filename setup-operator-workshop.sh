6796466b0179bea9775e25855dc2f3ab7ce91671
Peter Nagy <>
2019-10-22 14:43:56 +0200

    fix path issues

--- setup-operator-workshop.sh
+++ setup-operator-workshop.sh
@@ -5,14 +5,14 @@
 
 echo "Setup environment for WebLogic Kubernetes Operator Workshop..."
 
-curl -LSs https://raw.githubusercontent.com/nagypeter/vmcontrol/scripts/master/bin/install-docker.sh | bash
+curl -LSs https://raw.githubusercontent.com/nagypeter/vmcontrol/master/scripts/install-docker.sh | bash
 
-curl -LSs https://raw.githubusercontent.com/nagypeter/vmcontrol/scripts/master/bin/install-oci.sh | bash
+curl -LSs https://raw.githubusercontent.com/nagypeter/vmcontrol/master/scripts/install-oci.sh | bash
 
-curl -LSs https://raw.githubusercontent.com/nagypeter/vmcontrol/scripts/master/bin/install-kubectl.sh | bash
+curl -LSs https://raw.githubusercontent.com/nagypeter/vmcontrol/master/scripts/install-kubectl.sh | bash
 
-curl -LSs https://raw.githubusercontent.com/nagypeter/vmcontrol/scripts/master/bin/install-git.sh | bash
+curl -LSs https://raw.githubusercontent.com/nagypeter/vmcontrol/master/scripts/install-git.sh | bash
 
-curl -LSs https://raw.githubusercontent.com/nagypeter/vmcontrol/scripts/master/bin/clone-weblogic-kubernetes-operator.sh | bash
+curl -LSs https://raw.githubusercontent.com/nagypeter/vmcontrol/master/scripts/clone-weblogic-kubernetes-operator.sh | bash
 
 read -p "Setup complete for WebLogic Kubernetes Operator Workshop. Press [Enter] to close the window"
