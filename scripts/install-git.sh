fb5055235fb05c69fc3c20302b063b319172e9ca
Peter Nagy <nagypeter@users.noreply.github.com>
2019-11-07 22:29:54 +0100

    for oracle linux

--- scripts/install-git.sh
+++ scripts/install-git.sh
@@ -3,6 +3,7 @@
 #usage:
 # curl -LSs https://raw.githubusercontent.com/nagypeter/vmcontrol/scripts/master/bin/install-git.sh | bash
 
-sudo dnf install -y git
+# sudo dnf install -y git
+sudo yum install -y git
 
 git version
