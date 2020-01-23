61d2bf893ffa30c6f5e3aea8f58b91e23bcb6305
Peter Nagy <>
2020-01-23 01:54:15 +0100

    decrease the cron job frequency to 5 minutes

--- scripts/post-config-vm.sh
+++ scripts/post-config-vm.sh
@@ -3,4 +3,4 @@
 # POST config the VM
 
 # create crontab to clean packages
-(crontab -l 2>/dev/null | fgrep -v "*/10 * * * * pkcon refresh force -c -1 >/dev/null 2>&1"; echo "*/10 * * * * pkcon refresh force -c -1 >/dev/null 2>&1") | crontab -
+(crontab -l 2>/dev/null | fgrep -v "*/5 * * * * pkcon refresh force -c -1 >/dev/null 2>&1"; echo "*/5 * * * * pkcon refresh force -c -1 >/dev/null 2>&1") | crontab -
