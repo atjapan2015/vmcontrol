9cc3df25f5ef7f593e6d067deea58e924a7ece6e
atjapan2015 <atjapan2015@yahoo.co.jp>
2020-02-12 09:52:07 +0000

    update

--- scripts/clone-weblogic-kubernetes-operator.sh
+++ scripts/clone-weblogic-kubernetes-operator.sh
@@ -36,7 +36,7 @@ if [ ! -e ${GITLOCALFOLDER} ]; then
   echo "Cloning remote repository from ${GIT_URL} to ${GITLOCALFOLDER}..."
   git clone ${GIT_URL} -b ${BRANCH}
   cd ${GITLOCALFOLDER}
-  git checkout -b ${BRANCH} ${TAG}
+  git checkout ${TAG}
 else
   echo "Update remote repository from ${GIT_URL} to ${GITLOCALFOLDER}..."
   cd ${GITLOCALFOLDER}
