05aeba2142560d1ea83bad75851c666caf7f2da3
atjapan2015 <atjapan2015@users.noreply.github.com>
2020-02-12 08:47:34 +0000

    update to atjapan2015

--- setup-operator-workshop.sh
+++ setup-operator-workshop.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
 #usage:
-# bash <(curl -s https://raw.githubusercontent.com/nagypeter/vmcontrol/master/setup-operator-workshop.sh)
+# bash <(curl -s https://raw.githubusercontent.com/atjapan2015/vmcontrol/master/setup-operator-workshop.sh)
 
 echo "Setup environment for WebLogic Kubernetes Operator Workshop..."
 
 #clean
-curl -LSs https://raw.githubusercontent.com/nagypeter/vmcontrol/master/scripts/clean-vm.sh | bash
+curl -LSs https://raw.githubusercontent.com/atjapan2015/vmcontrol/master/scripts/clean-vm.sh | bash
 
-curl -LSs https://raw.githubusercontent.com/nagypeter/vmcontrol/master/scripts/config-vm.sh | bash
+curl -LSs https://raw.githubusercontent.com/atjapan2015/vmcontrol/master/scripts/config-vm.sh | bash
 
-curl -LSs https://raw.githubusercontent.com/nagypeter/vmcontrol/master/scripts/install-oci.sh | bash
+curl -LSs https://raw.githubusercontent.com/atjapan2015/vmcontrol/master/scripts/install-oci.sh | bash
 
-curl -LSs https://raw.githubusercontent.com/nagypeter/vmcontrol/master/scripts/install-kubectl.sh | bash
+curl -LSs https://raw.githubusercontent.com/atjapan2015/vmcontrol/master/scripts/install-kubectl.sh | bash
 
-curl -LSs https://raw.githubusercontent.com/nagypeter/vmcontrol/master/scripts/install-git.sh | bash
+curl -LSs https://raw.githubusercontent.com/atjapan2015/vmcontrol/master/scripts/install-git.sh | bash
 
-curl -LSs https://raw.githubusercontent.com/nagypeter/vmcontrol/master/scripts/clone-weblogic-kubernetes-operator.sh | bash
+curl -LSs https://raw.githubusercontent.com/atjapan2015/vmcontrol/master/scripts/clone-weblogic-kubernetes-operator.sh | bash
 
 echo "Setup is complete for WebLogic Kubernetes Operator Workshop."
 
